73b959b02f2f657ea79d8a2695a749928d4e063d
Leonardo Costa <leoocosta1209@gmail.com>
2026-07-09 20:52:04 -0300

    fix: Ajuste visualização contas a pagar

--- web/static/js/contas_pagar/visualizarContaPagar.js
+++ web/static/js/contas_pagar/visualizarContaPagar.js
@@ -10,7 +10,9 @@ export function setupVisualizarContaPagar() {
         document.getElementById('view_descricao').textContent = debito.descricao;
         document.getElementById('view_nr_documento').textContent = debito.nr_documento || '-';
         document.getElementById('view_nr_nota_fiscal').textContent = debito.nr_nota_fiscal || '-';
-        document.getElementById('view_valor').textContent = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(debito.valor_original);
+        document.getElementById('view_valor_original').textContent = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(debito.valor_original);
+        document.getElementById('view_saldo_restante').textContent = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(debito.saldo_restante);
+        document.getElementById('view_dt_pagamento').textContent = debito.dt_pagamento ? debito.dt_pagamento.substring(0, 10).split('-').reverse().join('/') : '-';
         document.getElementById('view_dt_entrada').textContent = debito.dt_entrada ? debito.dt_entrada.substring(0, 10).split('-').reverse().join('/') : '-';
         document.getElementById('view_dt_vencimento').textContent = debito.dt_vencimento ? debito.dt_vencimento.substring(0, 10).split('-').reverse().join('/') : '-';
         document.getElementById('view_parcela').textContent = `${debito.nr_parcela} / ${debito.nr_total_parcelas}`;
