78012cde8a22fc9dc8a5bfe70e8050d0302b21ab
Leonardo Costa <leoocosta1209@gmail.com>
2026-07-14 21:42:40 -0300

    fix: AJuste leitura requisição controller entrada de estoque

--- internal/controller/entrada_estoque.go
+++ internal/controller/entrada_estoque.go
@@ -1,9 +1,9 @@
 package controller
 
 import (
+	"encoding/json"
 	"gestao/internal/model"
 	"gestao/internal/service"
-	"gestao/pkg/requisicao"
 	"gestao/pkg/resposta"
 	"net/http"
 	"strconv"
@@ -32,8 +32,8 @@ func (c *EstoqueController) EntradaEstoque(w http.ResponseWriter, r *http.Reques
 	entrada.IDEstoque = uint64(idEstoque)
 	entrada.IDUsuario = idUsuario
 
-	if err := requisicao.ProcessarRequisicao(w, r, &entrada); err != nil {
-		resposta.Padrao(w, http.StatusBadRequest, map[string]string{"erro": err.Error()})
+	if err := json.NewDecoder(r.Body).Decode(&entrada); err != nil {
+		resposta.Padrao(w, http.StatusBadRequest, map[string]string{"erro": "Erro ao decodificar dados: " + err.Error()})
 		return
 	}
 
